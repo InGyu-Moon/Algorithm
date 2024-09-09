3c431f60e9898f38d462f4f19a460732613009af
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2024-09-09 09:44:18 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 15760 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer token;
+
+	static int n, m;
+	static boolean[] visited;
+	static int[] selected;
+
+	public static void permutation(int depth, int curr) {
+		// 기저조건
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				output.append(selected[i]).append(" ");
+			}
+			output.append("\n");
+			return;
+		}
+		// 재귀조건
+		for (int i = curr; i < n; i++) {
+			selected[depth] = i + 1;
+			permutation(depth + 1, i);
+		}
+
+	}
+
+	public static void main(String args[]) throws Exception {
+		token = new StringTokenizer(input.readLine());
+		n = Integer.parseInt(token.nextToken());
+		m = Integer.parseInt(token.nextToken());
+		visited = new boolean[n];
+		selected = new int[m];
+		permutation(0, 0);
+		System.out.println(output);
+	}
+}
