b2c9aa5949e2eb51fb08f12494ab4d55ec034cc4
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2024-09-17 21:33:44 +0900

    [Silver II] Title: N과 M (12), Time: 108 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer token;
+
+	static int n, m;
+	static int[] arr;
+	static int[] selected;
+	static boolean[] visited;
+
+	public static void sol(int depth,int curr) {
+		if (depth == m) {
+			for (int i = 0; i < m; i++) {
+				output.append(selected[i]).append(" ");
+			}
+			output.append("\n");
+			return;
+		}
+		int temp = -1;
+		for (int i = curr; i < n; i++) {
+			if (temp == arr[i])
+				continue;
+			temp = arr[i];
+			selected[depth] = arr[i];
+			sol(depth + 1,i);
+		}
+	}
+
+	public static void init() throws Exception {
+		token = new StringTokenizer(input.readLine());
+		n = Integer.parseInt(token.nextToken());
+		m = Integer.parseInt(token.nextToken());
+		arr = new int[n];
+		selected = new int[m];
+		visited = new boolean[n];
+		token = new StringTokenizer(input.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(token.nextToken());
+		}
+		Arrays.sort(arr);
+	}
+
+	public static void main(String[] args) throws Exception {
+		init();
+		sol(0,0);
+		System.out.println(output);
+	}
+}
