16fbfa22ee15c46ff1ca15c3f88570796727cab8
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2024-11-12 12:03:38 +0900

    [D4] Title: 햄스터, Time: 471 ms, Memory: 24,396 KB -BaekjoonHub

--- SWEA/D4/8275. 햄스터/햄스터.java
+++ SWEA/D4/8275. 햄스터/햄스터.java
@@ -3,85 +3,105 @@ import java.util.*;
 
 public class Solution {
 
-	private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	private static StringTokenizer token;
-	private static StringBuilder output = new StringBuilder();
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer token;
 
-	private static int N, X, M;
-	private static boolean flag;
-	private static int selected[];
-	private static int arr[][];
+	static int n, x, m;
+	static Pair[] pairs;
 
-	private static boolean check() {
-		for (int[] a : arr) {
-			int start = a[0] - 1;
-			int end = a[1] - 1;
-			int maxSum = a[2];
+	static int[] selected;
+
+	static List<int[]> ans;
+
+	public static boolean check(int[] selected) {
+		for (Pair pair : pairs) {
 			int sum = 0;
-			for (int i = start; i <= end; i++) {
+			for (int i = pair.l - 1; i < pair.r; i++) {
 				sum += selected[i];
 			}
-			if (sum != maxSum) {
+			if (sum != pair.s) {
 				return false;
 			}
 		}
-
 		return true;
 	}
 
-	private static void sol(int index) {
-		if (index < 0) {
-			if (check())
-				flag = true;
+	public static void dfs(int depth) {
+		// 기저조건
+		if (depth == n) {
+			// 조건 확인
+			if (check(selected)) {
+//				ans.add(selected);
+				ans.add(selected.clone());
+			}
 			return;
 		}
-
-		for (int i = X; i >= 0; i--) {
-			selected[index] = i;
-			sol(index - 1);
-			if (flag)
-				return;
+		// 반복 조건
+		for (int i = x; i >= 0; i--) {
+			selected[n - 1 - depth] = i;
+			dfs(depth + 1);
 		}
+
+	}
+
+	public static void sol() {
+		dfs(0);
+		if (ans.size() == 0) {
+			output.append("-1\n");
+		} else {
+//			Collections.sort(ans, (a, b) -> {
+//				for (int i = 0; i < a.length; i++) {
+//					return 1;
+//				}
+//				return 0;
+//			});
+			int[] temp = ans.get(0);
+			for (int i = 0; i < n; i++) {
+				output.append(temp[i]).append(" ");
+			}
+			output.append("\n");
+		}
+	}
+
+	public static void init() throws Exception {
+		token = new StringTokenizer(input.readLine());
+		n = Integer.parseInt(token.nextToken());
+		x = Integer.parseInt(token.nextToken());
+		m = Integer.parseInt(token.nextToken());
+		pairs = new Pair[m];
+		for (int i = 0; i < m; i++) {
+			token = new StringTokenizer(input.readLine());
+			int l = Integer.parseInt(token.nextToken());
+			int r = Integer.parseInt(token.nextToken());
+			int s = Integer.parseInt(token.nextToken());
+			pairs[i] = new Pair(l, r, s);
+		}
+
+		selected = new int[n];
+		ans = new ArrayList<int[]>();
+
 	}
 
 	public static void main(String[] args) throws Exception {
 		int T = Integer.parseInt(input.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			token = new StringTokenizer(input.readLine());
-			N = Integer.parseInt(token.nextToken());
-			X = Integer.parseInt(token.nextToken());
-			M = Integer.parseInt(token.nextToken());
-
-			selected = new int[N];
-			arr = new int[M][3];
-			flag = false;
-
-			for (int idx = 0; idx < M; idx++) {
-				token = new StringTokenizer(input.readLine());
-				int i = Integer.parseInt(token.nextToken());
-				int r = Integer.parseInt(token.nextToken());
-				int s = Integer.parseInt(token.nextToken());
-
-				arr[idx][0] = i;
-				arr[idx][1] = r;
-				arr[idx][2] = s;
-
-			}
-
-			sol(N - 1);
-
-			output.append("#").append(tc).append(" ");
-			if (flag) {
-				for (int i = 0; i < N; i++) {
-					output.append(selected[i]);
-					output.append(" ");
-				}
-			} else {
-				output.append(-1);
-			}
-			output.append("\n");
+		for (int t = 1; t <= T; t++) {
+			output.append("#").append(t).append(" ");
+			init();
+			sol();
 		}
 		System.out.println(output);
 	}
 
+	static class Pair {
+		int l;
+		int r;
+		int s;
+
+		public Pair(int l, int r, int s) {
+			this.l = l;
+			this.r = r;
+			this.s = s;
+		}
+	}
 }
