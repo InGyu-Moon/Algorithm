de113a1e435e4837f8b32f2433b59b2f95322cf0
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2024-11-12 17:36:48 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,36 +1,53 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer token;
 	static StringBuilder output = new StringBuilder();
-	static int n, m;
-	static boolean[] visited = new boolean[9];
-	static int[] selected = new int[8];
+	static StringTokenizer token;
 
-	private static void comb(int nthChoice, int curr) {
-		// 기저
-		if (nthChoice >= m) {
-			for (int i = 0; i < m; i++) {
-				output.append(selected[i]).append(" ");
+	static int n, m;
+
+	static int[] selected;
+	static boolean[] visited;
+
+	public static void dfs(int depth, int next) {
+		// 종료
+		if (depth == m) {
+			for (int num : selected) {
+				output.append(num).append(" ");
 			}
 			output.append("\n");
 			return;
 		}
-		// 재귀
-		for (int i = curr; i <= n; i++) {
-			selected[nthChoice] = i;			
-			comb(nthChoice + 1, i + 1);
+		// 반복
+		for (int i = next; i < n; i++) {
+			if (visited[i])
+				continue;
+			visited[i] = true;
+			selected[depth] = i + 1;
+			dfs(depth + 1, i + 1);
+			visited[i] = false;
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void init() throws IOException {
+		// 입력
 		token = new StringTokenizer(input.readLine());
 		n = Integer.parseInt(token.nextToken());
 		m = Integer.parseInt(token.nextToken());
-		comb(0, 1);
+
+		// 초기화
+		selected = new int[m];
+		visited = new boolean[n + 1];
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		init();
+		dfs(0, 0);
 		System.out.println(output);
 	}
+
 }
