025031aa10c10c0810a2b55c3b7380172b225642
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2024-12-22 03:41:05 +0900

    [Silver IV] Title: 문자열 집합, Time: 380 ms, Memory: 31076 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -6,29 +6,50 @@ public class Main {
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer token;
 
-	static int n, m, ans;
-	static List<String> strList;
+	static int n, m;
+	static List<String> list, check;
+
+	static int ans;
+
+	public static void sol() {
+		for (String str : check) {
+			int low = 0;
+			int high = n - 1;
+			while (low <= high) {
+				int mid = (low + high) / 2;
+				if (list.get(mid).equals(str)) {
+					ans++;
+					break;
+				}
+
+				if (list.get(mid).compareTo(str) < 0) {
+					low = mid + 1;
+				} else {
+					high = mid - 1;
+				}
+			}
+		}
+	}
 
 	public static void init() throws Exception {
 		token = new StringTokenizer(input.readLine());
 		n = Integer.parseInt(token.nextToken());
 		m = Integer.parseInt(token.nextToken());
+		list = new ArrayList<>();
+		check = new ArrayList<>();
 		ans = 0;
-		strList = new ArrayList<>();
 		for (int i = 0; i < n; i++) {
-			strList.add(input.readLine());
+			list.add(input.readLine());
 		}
 		for (int i = 0; i < m; i++) {
-			String temp = input.readLine();
-			for (String str : strList) {
-				if (str.equals(temp))
-					ans++;
-			}
+			check.add(input.readLine());
 		}
+		Collections.sort(list);
 	}
 
 	public static void main(String[] args) throws Exception {
 		init();
+		sol();
 		System.out.println(ans);
 	}
 
