2882f1a4239eb8dbc9fc6819443785d43b95083b
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2025-01-22 00:59:03 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -11,49 +11,42 @@ public class Main {
 	static int[] dx = { 0, 1, 0, -1 };
 
 	static int n, m;
-	static int[][] arr; // 0 -> 청소 x, 1 -> 벽
+	static int[][] arr;
 	static int y, x, dir;
 	static int ans;
 
 	public static void sol() {
 		while (true) {
-			// 현재 칸 청소
 			if (arr[y][x] == 0) {
 				ans++;
 				arr[y][x] = -1;
 			}
-			// 주변 4칸 확인 (청소 안된칸 있나)
 			boolean allClean = true;
 			for (int i = 0; i < 4; i++) {
 				int ny = y + dy[i];
 				int nx = x + dx[i];
-				if (arr[ny][nx] == 0 && allClean) { // 청소 안된칸
+				if (arr[ny][nx] == 0 && allClean) {
 					allClean = false;
 				}
 			}
 
-			if (allClean) { // 전부 청소가 되어있음
-				// 바라보는 방향 뒤로 후진
-				dir += 2;
-				dir %= 4;
-				int ny = y + dy[dir];
-				int nx = x + dx[dir];
-				if (arr[ny][nx] == 1) { // 벽이면 정지
+			if (allClean) {
+				int ny = y + dy[(dir+2)%4];
+				int nx = x + dx[(dir+2)%4];
+				if (arr[ny][nx] == 1) {
 					System.out.println(ans);
 					return;
 				}
 				y = ny;
 				x = nx;
-				dir += 2;
-				dir %= 4;
-			} else { // 청소 안된 부분이 있음
-				// 반시계로 돌면서 청소되지 않은 칸이 있으면 1칸 전진
+			} else {
+
 				for (int i = 0; i < 4; i++) {
 					dir += 3;
 					dir %= 4;
 					int ny = y + dy[dir];
 					int nx = x + dx[dir];
-					if (arr[ny][nx] == 0) { // 청소 안되어 있는 부분 찾음
+					if (arr[ny][nx] == 0) {
 						y = ny;
 						x = nx;
 						break;
