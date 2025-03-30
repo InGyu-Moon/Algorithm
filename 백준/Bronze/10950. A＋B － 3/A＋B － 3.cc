67f674db5625596ca5338ddeb8b446a9277ef7db
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2025-03-30 22:00:44 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+//#include<algorithm>
+using namespace std;
+int main() {
+
+	int input;
+	int a, b;
+
+	
+	cin >> input;
+	for (int i = 0; i < input; i++) {
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+	
+
+
+	return 0;
+}
