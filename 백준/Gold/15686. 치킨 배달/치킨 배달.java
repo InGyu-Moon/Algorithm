958009c3971933750b00e04b5cd6b242f15d253a
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2025-10-07 01:22:57 +0900

    [Gold V] Title: 치킨 배달, Time: 224 ms, Memory: 20080 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer token;
+
+	static int n, m;
+	static int[][] arr;
+	static List<int[]> chicken = new ArrayList<>();
+	static int cnt;
+	static boolean[] selected;
+	static int select = 0;
+	static int min = Integer.MAX_VALUE;
+
+	public static void print() {
+		for (int i = 0; i < cnt; i++) {
+			if (selected[i])
+				System.out.print("1 ");
+			else
+				System.out.print("0 ");
+		}
+		System.out.println();
+	}
+
+	public static int sol() {
+		int sum = 0;
+		List<int[]> list = new ArrayList<>();
+		for (int i = 0; i < cnt; i++) {
+			if (selected[i]) {
+				list.add(chicken.get(i));
+			}
+		}
+		boolean flag = true;
+		if (list.size() == 0)
+			flag = false;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				int min = Integer.MAX_VALUE;
+				if (arr[i][j] != 1)
+					continue;
+				for (int[] curr : list) {
+					int y = curr[0];
+					int x = curr[1];
+					int diff = Math.abs(y - i) + Math.abs(x - j);
+//					System.out.println("치킨 거리: " + diff);
+					min = Math.min(min, diff);
+				}
+				sum += min;
+//				System.out.println("sum: " + sum + " ====");
+			}
+		}
+		if (!flag)
+			return -1;
+		return sum;
+	}
+
+	public static void subset(int depth) {
+		if (select > m)
+			return;
+
+		if (depth == cnt) {
+			if (select != m)
+				return;
+//			print();
+			int diff = sol();
+			if (diff != -1) {
+//				System.out.println("diff: " + diff);
+				min = Math.min(min, diff);
+			}
+			return;
+		}
+		selected[depth] = false;
+		subset(depth + 1);
+		selected[depth] = true;
+		select++;
+		subset(depth + 1);
+		select--;
+	}
+
+	public static void init() throws Exception {
+		token = new StringTokenizer(input.readLine());
+		n = Integer.parseInt(token.nextToken());
+		m = Integer.parseInt(token.nextToken());
+		arr = new int[n + 1][n + 1];
+		for (int i = 1; i <= n; i++) {
+			token = new StringTokenizer(input.readLine());
+			for (int j = 1; j <= n; j++) {
+				arr[i][j] = Integer.parseInt(token.nextToken());
+				if (arr[i][j] == 2) {
+					chicken.add(new int[] { i, j });
+				}
+			}
+		}
+		cnt = chicken.size();
+		selected = new boolean[cnt];
+	}
+
+	public static void main(String[] args) throws Exception {
+		init();
+		subset(0);
+		System.out.println(min);
+	}
+}
