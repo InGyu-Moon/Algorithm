be45072ed5164cee5335c3099b426826e5c32f1c
InGyu Moon <98271218+InGyu-Moon@users.noreply.github.com>
2026-03-28 20:02:34 +0900

    [Gold IV] Title: 알고스팟, Time: 140 ms, Memory: 16532 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer token;
+
+	static int[] dy = { 0, -1, 0, 1 };
+	static int[] dx = { 1, 0, -1, 0 };
+
+	static int INF = 987654321;
+
+	public static int n, m;
+	public static int[][] arr;
+	public static int[][] visited;
+	static int ans = Integer.MAX_VALUE;
+
+	public static Deque<int[]> que = new ArrayDeque<>();
+
+	public static void sol() {
+		que.add(new int[] { 1, 1 });
+		while (!que.isEmpty()) {
+			int[] top = que.poll();
+			int y = top[0];
+			int x = top[1];
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+				if (ny == 0 || nx == 0 || ny == n + 1 || nx == m + 1)
+					continue;
+				if (visited[y][x] + arr[ny][nx] >= visited[ny][nx])
+					continue;
+				visited[ny][nx] = visited[y][x] + arr[ny][nx];
+				que.add(new int[] { ny, nx });
+			}
+		}
+
+	}
+
+	public static void init() throws Exception {
+		token = new StringTokenizer(input.readLine());
+		m = Integer.parseInt(token.nextToken());
+		n = Integer.parseInt(token.nextToken());
+		arr = new int[n + 1][m + 1];
+		visited = new int[n + 1][m + 1];
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				visited[i][j] = INF;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			String temp = input.readLine();
+			for (int j = 1; j <= m; j++) {
+				arr[i][j] = temp.charAt(j-1) - '0';
+			}
+		}
+		// 초기화
+		visited[1][1] = 0;
+	}
+
+	public static void main(String[] args) throws Exception {
+		init();
+		sol();
+		System.out.println(visited[n][m]);
+	}
+}
